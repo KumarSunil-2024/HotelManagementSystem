d131d1c26b0ff23cc9a04b6bab0444c1363caf67
Sunil Kumar <sunilkmr.java.dev1611@@gmail.com>
2026-02-01 17:08:27 +0530

    Convert frontend to normal folder

---
+++ frontend/src/component/common/Footer.jsx
@@ -0,0 +1,13 @@
+const FooterComponent = () => {
+
+
+    return (
+        <footer>
+            <span className="my-footer">
+                Sunil Hotel — Ghar Jaisa Araam  {new Date().getFullYear()}
+            </span>
+        </footer>
+    );
+};
+
+export default FooterComponent;
